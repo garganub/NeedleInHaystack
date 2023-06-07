c3e661b5a50c073c5f896fce927eae99c78cda41
Anubhav Garg <garganub@usc.edu>
2023-06-07 23:26:06 +0000

    Pending changes exported from your codespace

--- haystack.py
+++ haystack.py
@@ -12,15 +12,14 @@ def findNeedle(haystack, needle):
                     print("We have entered the match case, and it is potentially beginning of haystack. The Needle, Haystack letter combo is " + str(Nletter) + str(Hletter))
                     start_index = H_index_counter
                 elif N_index_counter == len(needle) - 1:
-                    print("This means we should have found the needle")
-                    print("Should be returning " + str(start_index))
+                    print("Start index is found, should be returning " + str(start_index))
                     return start_index
                 H_index_counter += 1
                 if H_index_counter < len(haystack):    
                     Hletter = haystack[H_index_counter]
             else:
                 start_index = -1
-                print("We know it is not the beginning of Needle. The haystack needle pair is " + Hletter + Nletter)
+                print("We know that this Needle character it is not the beginning of Needle. The haystack needle pair is " + Hletter + Nletter)
                 break
             N_index_counter += 1 
         N_index_counter = 0        
